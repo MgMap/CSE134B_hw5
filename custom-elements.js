a2d4e470f38e59a6461de1e4e2787f89e262c2fb
MgMap <minaungpaing.pp@gmail.com>
2025-11-03 23:07:29 -0800

    add profile pic

--- custom-elements.js
+++
@@ -1,9 +0,0 @@
-// Hello World Custom Element - logs to console only, does not modify DOM
-class HelloWorld extends HTMLElement {
-    connectedCallback() {
-        console.log('Hello world! This is a custom HTML element.');
-    }
-}
-
-// Register the custom element
-customElements.define('hello-world', HelloWorld);
